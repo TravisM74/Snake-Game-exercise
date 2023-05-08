e9fe62d9f326a434198f9c1d6963aa429b139e26
TravisM74 <117643403+TravisM74@users.noreply.github.com>
2023-05-08 13:54:03 +0300

    initial commit of files
    the classic snake game

---
+++ src/OptionsFrame.java
@@ -0,0 +1,19 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class OptionsFrame extends JFrame {
+	
+	OptionsFrame(){
+		
+	
+	OptionsFrame OptionsPanel = new OptionsFrame();
+	
+	this.add(OptionsPanel);
+	this.setTitle("Snake Options");
+	this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+	this.setResizable(false);
+	this.pack();
+	this.setVisible(true);
+	this.setLocationRelativeTo(null);
+	}
+}
